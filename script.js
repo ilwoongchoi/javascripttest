1abb7945225c2d5e37857d5876a057bf8e592737
ilwoongchoi <104112060+ilwoongchoi@users.noreply.github.com>
2024-01-26 15:58:01 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function toggleNavbar() {
+    var navbarLinks = document.getElementById("navbarLinks");
+    if (navbarLinks.style.display === "block") {
+        navbarLinks.style.display = "none";
+    } else {
+        navbarLinks.style.display = "block";
+    }
+}
+
+window.addEventListener("resize", function () {
+    var navbarLinks = document.getElementById("navbarLinks");
+    if (window.innerWidth > 768) {
+        navbarLinks.style.display = "block";
+    } else {
+        navbarLinks.style.display = "none";
+    }
+});
